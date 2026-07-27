8e108466724ac80a77152440c0726d8685b1ebe1
Gabriel Caixeta Romero <gabrielcaixetaromero@gmail.com>
2026-07-27 16:11:37 -0300

    Ajuste no run

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const geist = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  weight: ["400", "500", "600", "700", "900"],
 });
 
 export const metadata: Metadata = {
